48c94e901573097e9759e6263c0876586ea87daf
Carlosmagno23 <deltamagno01@gmail.com>
2025-08-15 11:51:48 -0300

    projeto carregado

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import { ThemeProvier } from './assets/ThemeContext.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvier>
+      <App />
+    </ThemeProvier>
+    
+  </StrictMode>,
+)
